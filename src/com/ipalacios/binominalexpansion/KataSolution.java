9132e0f8c2f951646b72b7bd729133b0bf7dcff6
Iván Palacios <ivan@ipalacios.local>
2020-06-19 20:02:14 +0200

    Finalizado binomial expansion pendiente de refactorizar

--- src/com/ipalacios/binominalexpansion/KataSolution.java
+++ src/com/ipalacios/binominalexpansion/KataSolution.java
@@ -1,99 +1,105 @@
 package com.ipalacios.binominalexpansion;
 
+import java.util.function.BinaryOperator;
+
 // Binomial expansion
 // https://www.codewars.com/kata/540d0fdd3b6532e5c3000b5b/train/java
 public class KataSolution {
 
-    public static int triangle (int n, int k) {
-        if (k == 0 || k == n) {
-            return 1;
+    public static class Binomial {
+        // Represents a binomial (ax+b)^n
+        private int a = 1, b = 1;
+        private int n;
+        private char variable;
+        private long[] values;
+
+        public Binomial(String expression) {
+
+            String[] splitted = expression.split("\\^");
+            this.n = Integer.valueOf(splitted[1]).intValue();
+            String binomial = splitted[0].replaceAll("[(,)]","");
+            int index = 0;
+            if (binomial.charAt(index) == '+') {
+                index++;
+            }
+            if (binomial.charAt(index) == '-') {
+                this.a *= -1;
+                index++;
+            }
+
+            String coeficientStr = "";
+            while(String.valueOf(binomial.charAt(index)).matches("[0-9]")) {
+                coeficientStr += binomial.charAt(index);
+                index++;
+            }
+            if (!coeficientStr.isEmpty()) {
+                this.a *= Integer.valueOf(coeficientStr).intValue();
+            }
+            this.variable += binomial.charAt(index++);
+
+            if (binomial.charAt(index) == '+') {
+                index++;
+            }
+            if (binomial.charAt(index) == '-') {
+                this.b *= -1;
+                index++;
+            }
+            coeficientStr = "";
+            while(index < binomial.length() && String.valueOf(binomial.charAt(index)).matches("[0-9]")) {
+                coeficientStr += binomial.charAt(index);
+                index++;
+            }
+            this.b *= Integer.valueOf(coeficientStr).intValue();
         }
-        return triangle(n - 1 , k - 1) + triangle (n -1, k);
+
+        public Binomial compute() {
+            this.values  = new long[n+1];
+
+            for (int i = 0; i <= n; i++) {
+                this.values[i] = (long)Math.pow(a, n - i) * (long)Math.pow(b, i);
+                this.values[i] *= triangle(n, i);
+            }
+            return this;
+        }
+
+        public String toString() {
+            StringBuilder str = new StringBuilder();
+
+
+            for (int i = 0; i <= n; i++) {
+                if (this.values[i] != 0) {
+                    if (this.values[i] > 0 && i > 0) {
+                        str.append("+");
+                    }
+                    if (this.values[i] == -1 && i < n) {
+                        str.append("-");
+                    }
+                    if (this.values[i] > 1 || this.values[i] < -1 || i == n) {
+                        str.append(String.valueOf(this.values[i]));
+                    }
+                    if (n - i > 0) {
+                        str.append(variable);
+                    }
+                    if (n - i > 1) {
+                        str.append("^");
+                        str.append(String.valueOf(n - i));
+                    }
+                }
+            }
+
+            return str.toString();
+        }
+
+        private int triangle (int n, int k) {
+            if (k == 0 || k == n) {
+                return 1;
+            }
+            return triangle(n - 1 , k - 1) + triangle (n -1, k);
+        }
+
     }
 
-
-
     public static String expand(String expr) {
-
-
-        int n = 0;
-        int a = 1;
-        int b = 1;
-        String variable = "";
-
-        // TODO: parsear la entrada
-        String[] splitted = expr.split("\\^");
-        n = Integer.valueOf(splitted[1]).intValue();
-        String binomial = splitted[0].replaceAll("[(,)]","");
-        int index = 0;
-        if (binomial.charAt(index) == '+') {
-            index++;
-        }
-        if (binomial.charAt(index) == '-') {
-            a *= -1;
-            index++;
-        }
-
-        String coeficientStr = "";
-        while(String.valueOf(binomial.charAt(index)).matches("[0-9]")) {
-            coeficientStr += binomial.charAt(index);
-            index++;
-        }
-        if (!coeficientStr.isEmpty()) {
-            a *= Integer.valueOf(coeficientStr).intValue();
-        }
-        variable += binomial.charAt(index++);
-
-        if (binomial.charAt(index) == '+') {
-            index++;
-        }
-        if (binomial.charAt(index) == '-') {
-            b *= -1;
-            index++;
-        }
-        coeficientStr = "";
-        while(index < binomial.length() && String.valueOf(binomial.charAt(index)).matches("[0-9]")) {
-            coeficientStr += binomial.charAt(index);
-            index++;
-        }
-        b *= Integer.valueOf(coeficientStr).intValue();
-
-
-
-
-        // Calculo
-        int[] valores  = new int[n+1];
-
-                // Los coeficientes se van almacenando en un array de n + 1
-
-
-
-        for (int i = 0; i <= n; i++) {
-            valores[i] = (int)Math.pow(a, n - i) * (int)Math.pow(b, i);
-            valores[i] *= triangle(n, i);
-        }
-
-        // TODO: crear la salida a string
-
-        StringBuilder str = new StringBuilder();
-
-        for (int i = 0; i <= n; i++) {
-            if (valores[i] > 0 && i > 0) {
-                str.append("+");
-            }
-            if (valores[i] > 1) {
-                str.append(String.valueOf(valores[i]));
-            }
-            if (n - i > 0) {
-                str.append(variable);
-            }
-            if (n - i > 1) {
-                str.append("^");
-                str.append(String.valueOf(n-i));
-            }
-        }
-
-        String response = str.toString();
-       return response;
+        return new Binomial(expr).compute().toString();
     }
 }
--- test/com/ipalacios/SolutionTest.java
+++ test/com/ipalacios/SolutionTest.java
@@ -29,6 +29,7 @@ public class SolutionTest {
         assertEquals("625m^4+1500m^3+1350m^2+540m+81", KataSolution.expand("(5m+3)^4"));
         assertEquals("8x^3-36x^2+54x-27", KataSolution.expand("(2x-3)^3"));
         assertEquals("1", KataSolution.expand("(7x-7)^0"));
+        assertEquals("81t^2", KataSolution.expand("(9t-0)^2"));
     }
 
     @Test
@@ -36,6 +37,12 @@ public class SolutionTest {
         assertEquals("625m^4-1500m^3+1350m^2-540m+81", KataSolution.expand("(-5m+3)^4"));
         assertEquals("-8k^3-36k^2-54k-27", KataSolution.expand("(-2k-3)^3"));
         assertEquals("1", KataSolution.expand("(-7x-7)^0"));
+        assertEquals("-n^5-60n^4-1440n^3-17280n^2-103680n-248832", KataSolution.expand("(-n-12)^5"));
+    }
+
+    @Test
+    public void randomTest() {
+        assertEquals("282475249j^10+3228288560j^9+16602626880j^8+50598481920j^7+101196963840j^6+138784407552j^5+132175626240j^4+86318776320j^3+36993761280j^2+9395240960j+1073741824", KataSolution.expand("(-7j-8)^10"));
     }
 
 
